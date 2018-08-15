4a924e936232ce2a128cb8dff145df4934dc6ad3
cyndi088 <cyndi088@163.com>
2018-08-15 19:58:59 +0800

    抓cities

---
+++ DataAcquisition/spiders/cities.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+import geohash
+from ..items import CitiesItem
+
+
+class CitiesSpider(scrapy.Spider):
+    name = 'cities'
+    allowed_domains = ['www.ele.me/home/']
+    start_urls = ['https://www.ele.me/restapi/shopping/v1/cities']
+
+    def parse(self, response):
+        res = json.loads(response.text, encoding='gbk')
+        city = CitiesItem()
+        for i in res:
+            for data in res[i]:
+                city['abbr'] = data['abbr']
+                city['id'] = data['id']
+                city['latitude'] = data['latitude']
+                city['longitude'] = data['longitude']
+                city['geohash'] = geohash.encode(data['latitude'], data['longitude'])
+                city['name'] = data['name']
+                city['pinyin'] = data['pinyin']
+                yield city
--- DataAcquisition/spiders/eleme.py
+++ DataAcquisition/spiders/eleme.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 
 
 class ElemeSpider(scrapy.Spider):
     name = 'eleme'
     allowed_domains = ['www.ele.me/home/']
-    start_urls = ['http://www.ele.me/home//']
+    urls_list = []
+    district_list = ['临安区', '西湖区', '上城区', '下城区', '拱墅区', '建德市', '萧山区', '富阳区', '江干区', '桐庐县', '淳安县', '余杭区', '滨江区']
+    for district in district_list:
+        urls_list.append('https://www.ele.me/restapi/v2/pois?extras[]=count&geohash=wtmknpnr9yy3&keyword=%s&limit=20&type=nearby' % district)
+    start_urls = urls_list
 
     def parse(self, response):
-        pass
+        res = response.text
+        print('*********************************************')
+        print(json.loads(res, encoding='gbk')[0])
